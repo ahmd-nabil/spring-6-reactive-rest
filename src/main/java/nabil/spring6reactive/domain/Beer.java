5d17b13c20f0317926ab7afb2b682f1f573fc66b
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-06 22:43:04 +0300

    added Beer object

---
+++ src/main/java/nabil/spring6reactive/domain/Beer.java
@@ -0,0 +1,35 @@
+package nabil.spring6reactive.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+    @Id
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+    
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
