9ba993fcc2e599fe7ee72d0c402c49f5ac04f2b7
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-06 21:26:33 +0300

    first commit

---
+++ src/main/java/nabil/spring6reactive/Spring6ReactiveApplication.java
@@ -0,0 +1,13 @@
+package nabil.spring6reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveApplication.class, args);
+    }
+
+}
