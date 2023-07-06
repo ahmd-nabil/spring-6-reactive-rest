e890aacb2664baf607575cb29eed5969c829849b
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-06 21:51:02 +0300

    initializing database

---
+++ src/main/java/nabil/spring6reactive/config/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package nabil.spring6reactive.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Configuration
+public class DatabaseConfig {
+    @Value("classpath:/schema.sql")
+    Resource resource;
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(resource));
+        return initializer;
+    }
+}
