1968922828057e0d1817135dda14b6c57848ec36
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-06 22:55:04 +0300

    enable Auditing for auto setting create and lastModified dates

--- src/test/java/nabil/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/nabil/spring6reactive/repositories/BeerRepositoryTest.java
@@ -1,10 +1,12 @@
 package nabil.spring6reactive.repositories;
 
+import nabil.spring6reactive.config.DatabaseConfig;
 import nabil.spring6reactive.domain.Beer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -14,6 +16,7 @@ import java.math.BigDecimal;
  * @author Ahmed Nabil
  */
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
