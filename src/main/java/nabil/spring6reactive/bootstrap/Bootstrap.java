7a66d93e3a21a768926a7e8a258a1768145ebb7d
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-07 15:46:45 +0300

    populating database with beers

---
+++ src/main/java/nabil/spring6reactive/bootstrap/Bootstrap.java
@@ -0,0 +1,64 @@
+package nabil.spring6reactive.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import nabil.spring6reactive.domain.Beer;
+import nabil.spring6reactive.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Component
+@RequiredArgsConstructor
+public class Bootstrap implements CommandLineRunner {
+    private final BeerRepository beerRepository;
+    @Override
+    public void run(String... args) {
+        beerRepository.count().subscribe(count -> {
+            if(count == 0) {
+                populateBeers();
+            }
+        });
+    }
+
+
+    private void populateBeers() {
+        Beer beer1 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer2 = Beer.builder()
+                .beerName("Crank")
+                .beerStyle("Pale Ale")
+                .upc("12356222")
+                .price(new BigDecimal("11.99"))
+                .quantityOnHand(392)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .beerName("Sunshine City")
+                .beerStyle("IPA")
+                .upc("12356")
+                .price(new BigDecimal("13.99"))
+                .quantityOnHand(144)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        beerRepository.save(beer1).subscribe();
+        beerRepository.save(beer2).subscribe();
+        beerRepository.save(beer3).subscribe();
+    }
+}
