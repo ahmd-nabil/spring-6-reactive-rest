04588a634b5d25aaaf522aa1c10cf3cfc0028e6f
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-08 05:57:46 +0300

    add missing @Id annotation

--- src/main/java/nabil/spring6reactive/domain/Customer.java
+++ src/main/java/nabil/spring6reactive/domain/Customer.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class Customer {
+    @Id
     private Integer id;
     private String firstName;
     private String lastName;
