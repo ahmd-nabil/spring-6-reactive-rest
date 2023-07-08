cad3c6ce392cdb871a158671de1c0f8c928e6d2d
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-08 06:00:25 +0300

    added CustomerController endpoints & tested with postman

---
+++ src/main/java/nabil/spring6reactive/controllers/CustomerController.java
@@ -0,0 +1,62 @@
+package nabil.spring6reactive.controllers;
+
+import lombok.RequiredArgsConstructor;
+import nabil.spring6reactive.model.CustomerDTO;
+import nabil.spring6reactive.services.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+/**
+ * @author Ahmed Nabil
+ */
+
+@RequiredArgsConstructor
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+    private final static String CUSTOMER_ENDPOINT = "/api/v2/customers";
+    private final static String CUSTOMER_ENDPOINT_ID = CUSTOMER_ENDPOINT + "/{customerId}";
+
+    @GetMapping(CUSTOMER_ENDPOINT)
+    Flux<CustomerDTO> findAll() {
+        return customerService.findAll();
+    }
+
+    @GetMapping(CUSTOMER_ENDPOINT_ID)
+    Mono<CustomerDTO> findById(@PathVariable(name = "customerId") Integer id) {
+        return customerService.findById(id);
+    }
+
+    @PostMapping(CUSTOMER_ENDPOINT)
+    Mono<ResponseEntity<Void>> save(@RequestBody CustomerDTO customerDTO) {
+        return customerService
+                .save(customerDTO)
+                .map(savedDto ->
+                        ResponseEntity
+                                .created(URI.create("http://localhost:8080"+CUSTOMER_ENDPOINT+ "/" +savedDto.getId()))
+                                .build()
+                );
+    }
+
+    @PutMapping(CUSTOMER_ENDPOINT_ID)
+    Mono<ResponseEntity<Void>> update(@PathVariable(name = "customerId") Integer id,
+                                      @RequestBody CustomerDTO customerDTO) {
+        return customerService.update(id, customerDTO).then(Mono.just(ResponseEntity.noContent().build()));
+    }
+
+    @PatchMapping(CUSTOMER_ENDPOINT_ID)
+    Mono<ResponseEntity<Void>> patch(@PathVariable("customerId") Integer id,
+                                     @RequestBody CustomerDTO customerDTO) {
+        return customerService.patch(id, customerDTO).then(Mono.just(ResponseEntity.noContent().build()));
+    }
+
+    @DeleteMapping(CUSTOMER_ENDPOINT_ID)
+    Mono<ResponseEntity<Void>> deleteById(@PathVariable("customerId") Integer id) {
+        return customerService.deleteById(id).then(Mono.just(ResponseEntity.noContent().build()));
+    }
+}
